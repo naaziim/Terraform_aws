4f04dbdf1602aebf2b9267cb8a52e7d16e83a0aa
naaziim <118941370+naaziim@users.noreply.github.com>
2024-10-04 15:01:59 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,10 +2,10 @@ provider "aws" {
     region = "us-east-1"  
 }
 
-resource "aws_instance" "foo" {
+resource "aws_instance" "foo-nazim" {
   ami           = "ami-05fa00d4c63e32376" # us-west-2
   instance_type = "t2.micro"
   tags = {
-      Name = "TF-Instance"
+      Name = "TF-Instance-nazim"
   }
 }
